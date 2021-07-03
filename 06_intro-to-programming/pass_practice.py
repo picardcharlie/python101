67335ad6afba178ac199c90f44da070c7b108b24
picardcharlie <picardcharlie@gmail.com>
2021-07-02 22:23:42 -0500

    testing to see how this works

---
+++ 06_intro-to-programming/pass_practice.py
@@ -0,0 +1,11 @@
+# Learning the pass command in python and how it can be used.
+# Typing it all out so I get some coding practice in and remember how to write in vim.
+
+user_input = input("Whisper something: ")
+
+for char in user_input:
+    pass
+    # Make each character uppercase
+    # So it "shouts" it back
+
+print("shhh!")
---
+++ 06_intro-to-programming/print_practice.py
@@ -0,0 +1,26 @@
+# Tasks for this are...
+
+# Write a script that uses print() to display something to console.
+print("I should probably print out a number")
+print(88)
+print("That's a real lucky number!")
+
+# What happens when you try to print 42 with quotes.
+print(42)
+
+# What happens when you print Hello world! without quotes.
+# print(Hello world!)
+# you get a syntax error!
+
+# Can you perform calculations inside of print()
+print(34 + 35)
+# Yes you can, 2 + 2 is 4, quick math
+
+#How many error messages can I get and are they useful?
+print() #nothing
+print(None) #nothing
+# print("hello" + 2) #type error, must be a str
+# print("Hello" + False) #type error, not bool
+# print("No end quote) #syntax error
+print(print()) #nothing
+print("print()") #nothing

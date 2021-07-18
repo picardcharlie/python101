455dbf6307b2b1465f45bbbf6c6a06332440fd1a
picardcharlie <picardcharlie@gmail.com>
2021-07-18 17:11:18 -0500

    section 10 labs

---
+++ 10_operators-and-booleans/10_06_wool_and_bools.py
@@ -0,0 +1,9 @@
+# Write a code snippet that gives a name to a `sheep`
+# and uses a boolean value to define whether it has `wool` or not.
+
+sheep = "Marge"
+wool = False
+
+sheep2 = "Taggy"
+wool2 = True
+#I feel like this is where lists and arrays come in to play.
